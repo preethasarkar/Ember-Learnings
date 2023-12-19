8a64f50817361128c6de168bef92df0cae3cb98c
Preetha <preethasarkar2018@gmail.com>
2023-12-19 16:50:44 +0530

    Modified frontende files

--- EmberLearnings/App.js
+++ EmberLearnings/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View className="flex-1 justify-center items-center bg-black ">
+      <TouchableOpacity className="bg-orange-500 p-3 rounded-lg shadow-md shadow-gray-00">
+        <Text>Ember</Text>
+      </TouchableOpacity>
+      <StatusBar style="dark" />
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
